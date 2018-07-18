6a707e74c3dff9af5f8324caeab7d70408ccd5ec
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-17 23:10:49 -0500

    site is clicking through, photos not showing up, having trouble figuring out how to link up the photo controller and ejs files correctly

--- models/users.js
+++ models/users.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
 	username: {type: String},
 	password: {type: String, required: true},
+	// photos: [Photo.schema]
+
 });
 
 module.exports = mongoose.model('User', userSchema);
